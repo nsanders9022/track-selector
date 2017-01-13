ff0a057d3844c5fc6f4fda08e77006391ea768a1
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 09:42:55 -0800

    fix some small mistakes and add the first line of logic in the business side function

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
+var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) {
+  if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
+    var answer = "You should do some more research to learn more about this field and if it is a good fit for you";
+    return answer;
+  } else {
+    return "lajlkjljlkj"
+  }
+} //function end
+
+
+
+
 $(document).ready(function(){
   $("form#blank-form").submit(function(event){
-    $(".result").hide();
 
+    var computersAnswer = $("input:radio[name=work-preference]:checked").val();
+    var cssAnswer = $("input:radio[name=front-back]:checked").val();
+    var rubyAnswer = $("input:radio[name=size]:checked").val();
+    var phpAnswer = $("input:radio[name=products]:checked").val();
+    var javaCSharpAnswer = $("input:radio[name=tool]:checked").val();
 
-    var computersAnswer = $("input:radio[name=:work-preference:checked");
-    var cssAnswer = $("input:radio[name=:front-back:checked");
-    var rubyAnswer = $("input:radio[name=:size:checked");
-    var phpAnswer = $("input:radio[name=:products:checked");
-    var javaCSharpAnswer = $("input:radio[name=:tool:checked");
-
-    $("p#result").text(answer)
-    $(".result").show();
+    $("p#result").text(trackSelect(computersAnswer, cssAnswer, rubyAnswer, phpAnswer, javaCSharpAnswer))
   });
 });
