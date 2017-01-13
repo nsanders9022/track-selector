e028386ed013021c36456acc5bdfc1b8c2dfe722
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 11:28:30 -0800

    add text content to the returned result about each track option.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@ var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) 
   if (compPeople && design && smallLarge && appWeb && netAndroid ){
     if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
       return "You should do some more research to learn more about this field and if it is a good fit for you";
-
     } else if (compPeople === "people") {
       return "I suggest you look for a different career path since you don't like to work with computers"
     } else if (design === "front"){
-      return "CSS! This track"
+      return "Design/CSS! This track is perfect for you since you want to work on the front end of web programming. Here you get to learn about planning out the aesthetics of a page and selecting the most user friendly and effective styles"
     } else if (smallLarge === "small") {
-      return "Ruby............."
+      return "Ruby/Rails! Ruby's simplistic and naturalistic syntax makes this a fun language to learn and a popular language for startups and small companies to code it."
     } else if (appWeb === "web") {
-      return "PHP ....................."
+      return "PHP/Drupal! This track will prepare you for working on sites that are geared towards specific clients. These sites are content heavy and involve many different aspects, including form data and cookies."
     } else if (netAndroid ==="net"){
-      return "C#............"
+      return "C#/.NET! If you want to work for a large corporation or the government this track is right for you. Microsoft is a major employer that uses these languages. C# is used to develop a company's internal software as well as apps for clients and databases."
     } else {
-      return "Java ........."
+      return "Java/Android! This track teaches you all about Android. This operating system can be used to create all the apps you can think of."
     }
   } else {
     return "Please answer every question."
