dcf8f842ed132c0cb5919c14c67081e503aec4d3
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 10:09:07 -0800

    expand the business logic to include more if/else statements that return a result depending on the response to a particular question

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
 var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) {
   if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
-    var answer = "You should do some more research to learn more about this field and if it is a good fit for you";
-    return answer;
+    return "You should do some more research to learn more about this field and if it is a good fit for you";
+
+  } else if (compPeople === "people") {
+    return "I suggest you look for a different career path since you don't like to work with computers"
+  } else if (design === "front"){
+    return "CSS..................."
+  } else if (smallLarge === "small") {
+    return "Ruby............."
+  } else if (appWeb === "web") {
+    return "PHP ....................."
+  } else if (netAndroid ==="net"){
+    return "C#............"
   } else {
-    return "lajlkjljlkj"
+    return "Java ........."
   }
-} //function end
+}; //function end
 
 
 
 
 $(document).ready(function(){
   $("form#blank-form").submit(function(event){
-
-    var computersAnswer = $("input:radio[name=work-preference]:checked").val();
+    event.preventDefault();
+    var computersAnswer = $("input:radio[name=preference]:checked").val();
     var cssAnswer = $("input:radio[name=front-back]:checked").val();
     var rubyAnswer = $("input:radio[name=size]:checked").val();
     var phpAnswer = $("input:radio[name=products]:checked").val();
     var javaCSharpAnswer = $("input:radio[name=tool]:checked").val();
 
-    $("p#result").text(trackSelect(computersAnswer, cssAnswer, rubyAnswer, phpAnswer, javaCSharpAnswer))
+    $("#result").text(trackSelect(computersAnswer, cssAnswer, rubyAnswer, phpAnswer, javaCSharpAnswer));
   });
 });
