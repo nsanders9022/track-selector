a809d5d467490ad1c9668f9ad588c6f68faa419e
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 14:59:03 -0800

    add images for each language and the image appears when the corresponding language is a result of the questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+imageIcon = ""
+
 var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) {
   if (compPeople && design && smallLarge && appWeb && netAndroid ){
     if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
@@ -7,24 +9,29 @@ var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) 
     } else if (compPeople === "people") {
       return "I suggest you look for a different career path since you don't like to work with computers."
     } else if (design === "front"){
-      return "Design/CSS! This track is perfect for you since you want to work on the front end of web programming. Here you get to learn about planning out the aesthetics of a page and selecting the most user friendly and effective styles."
+      imageIcon = "design"
+      return "Design/CSS! This track is perfect for you since you want to work on the front end of web programming. Here you get to learn about planning out the aesthetics of a page and selecting the most user friendly and effective styles.";
     } else if (compPeople === "computers" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
-      return "You want a back end track like C#, Java, PHP or Ruby. Do some more research to find out what kinds of companies or types of products you'd like to work on to help give you an opinion on the factors that differentiate the languges."
+      return "You want a back end track like C#, Java, PHP or Ruby. Do some more research to find out what kinds of companies or types of products you'd like to work on to help give you an opinion on the factors that differentiate these languges.";
     } else if (smallLarge === "large" && appWeb === "undecided" && netAndroid === "undecided") {
-      return "You've narrowed down your options to C#, Java, and PHP because you want to work on the back end for a large company. Now figure out if you want to work on apps or on web products."
+      return "You've narrowed down your options to C#, Java, and PHP because you want to work on the back end for a large company. Now figure out if you want to work on apps or on web products.";
     } else if (appWeb === "app" && netAndroid === "undecided") {
-      return "You've now narrowed down your options to C# and Java because you want to have the ability to work on apps for large companies. Spend some time learning whether you would prefer to work with .NET or Android."
+      return "You've now narrowed down your options to C# and Java because you want to have the ability to work on apps for large companies. Spend some time learning whether you would prefer to work with .NET or Android.";
     } else if (smallLarge === "small") {
-      return "Ruby/Rails! Ruby's simplistic and naturalistic syntax makes this a fun language to learn and a popular language for startups and small companies to code it."
+      imageIcon = "ruby";
+      return "Ruby/Rails! Ruby's simplistic and naturalistic syntax makes this a fun language to learn and a popular language for startups and small companies to code it.";
     } else if (appWeb === "web") {
-      return "PHP/Drupal! This track will prepare you for working on sites that are geared towards specific clients. These sites are content heavy and involve many different aspects, including form data and cookies."
+      imageIcon = "php";
+      return "PHP/Drupal! This track will prepare you for working on sites that are geared towards specific clients. These sites are content heavy and involve many different aspects, including form data and cookies.";
     } else if (netAndroid ==="net"){
-      return "C#/.NET! If you want to work for a large corporation or the government this track is right for you. Microsoft is a major employer that uses these languages. C# is used to develop a company's internal software as well as apps for clients and databases."
+      imageIcon = "cSharp";
+      return "C#/.NET! If you want to work for a large corporation or the government this track is right for you. Microsoft is a major employer that uses these languages. C# is used to develop a company's internal software as well as apps for clients and databases.";
     } else {
-      return "Java/Android! This track teaches you all about Android. This operating system can be used to create all the apps you can think of."
+      imageIcon = "java";
+      return "Java/Android! This track teaches you all about Android. This operating system can be used to create all the apps you can think of.";
     }
   } else {
-    return "Please make sure you answer every question."
+    return "Please make sure you answer every question.";
   }
 };
 
@@ -39,12 +46,13 @@ $(document).ready(function(){
 
     var nameInput = $("#name").val();
 
-    // var cssImage = $("img#design");
-
     $("#result").text(trackSelect(computersAnswer, cssAnswer, rubyAnswer, phpAnswer, javaCSharpAnswer));
     $("#nameHere").text(nameInput);
 
+
+    $("#" + imageIcon).show();
+
     $("form#blank-form").hide();
-    $(".result").show()
+    $(".result").show();
   });
 });
