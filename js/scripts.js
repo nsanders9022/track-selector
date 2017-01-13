41c5ad1e162ae14d7f5421866590bd93fc216de1
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 09:19:07 -0800

    add user logic to the scripts file values are defined based on the questions that are answered the result is added to the paragraph on the HTML page

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("form#blank-form").submit(function(event){
+    var computersAnswer = $("input:radio[name=:work-preference:checked");
+    var cssAnswer = $("input:radio[name=:front-back:checked");
+    var rubyAnswer = $("input:radio[name=:size:checked");
+    var phpAnswer = $("input:radio[name=:products:checked");
+    var javaCSharpAnswer = $("input:radio[name=:tool:checked");
+
+    $("p#result").text(answer)
+  });
+});
