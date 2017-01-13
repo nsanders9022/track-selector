7d97d9fcb72b55eb48d0a6485a675e380e6d177d
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 10:26:37 -0800

    add styling to the text, hide the form when the results are displayed, add styling to the results well

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) {
-  if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
-    return "You should do some more research to learn more about this field and if it is a good fit for you";
+  if (compPeople && design && smallLarge && appWeb && netAndroid ){
+    if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
+      return "You should do some more research to learn more about this field and if it is a good fit for you";
 
-  } else if (compPeople === "people") {
-    return "I suggest you look for a different career path since you don't like to work with computers"
-  } else if (design === "front"){
-    return "CSS..................."
-  } else if (smallLarge === "small") {
-    return "Ruby............."
-  } else if (appWeb === "web") {
-    return "PHP ....................."
-  } else if (netAndroid ==="net"){
-    return "C#............"
+    } else if (compPeople === "people") {
+      return "I suggest you look for a different career path since you don't like to work with computers"
+    } else if (design === "front"){
+      return "CSS..................."
+    } else if (smallLarge === "small") {
+      return "Ruby............."
+    } else if (appWeb === "web") {
+      return "PHP ....................."
+    } else if (netAndroid ==="net"){
+      return "C#............"
+    } else {
+      return "Java ........."
+    }
   } else {
-    return "Java ........."
+    return "Please answer every question"
   }
-}; //function end
+};
 
 
 
@@ -30,5 +34,8 @@ $(document).ready(function(){
     var javaCSharpAnswer = $("input:radio[name=tool]:checked").val();
 
     $("#result").text(trackSelect(computersAnswer, cssAnswer, rubyAnswer, phpAnswer, javaCSharpAnswer));
+
+    $("form#blank-form").hide();
+    $(".result").show()
   });
 });
