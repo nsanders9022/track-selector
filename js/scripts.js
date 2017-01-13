b9a20dbedb5e63b83afd07d63d9d0267b8f19a34
Nicole Sanders <nsanders9022@gmail.com>
2017-01-13 15:09:42 -0800

    add more images to be shown for the results that are not specific languages (eg research and an error image)

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
-imageIcon = ""
+var imageIcon = ""
 
 var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) {
   if (compPeople && design && smallLarge && appWeb && netAndroid ){
     if (compPeople === "both" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
+      imageIcon = "research";
       return "You should do some more research to learn more about this field and if it is a good fit for you.";
     } else if (compPeople === "computers" && design === "undecided" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
+      imageIcon = "research";
       return "This field is right for you since you like to work with computers. However, it would be a good idea to spend more time on figuring out what path you want to go down.";
     } else if (compPeople === "people") {
+      imageIcon = "research";
       return "I suggest you look for a different career path since you don't like to work with computers."
     } else if (design === "front"){
       imageIcon = "design"
       return "Design/CSS! This track is perfect for you since you want to work on the front end of web programming. Here you get to learn about planning out the aesthetics of a page and selecting the most user friendly and effective styles.";
     } else if (compPeople === "computers" && smallLarge === "undecided" && appWeb === "undecided" && netAndroid === "undecided") {
+      imageIcon = "research";
       return "You want a back end track like C#, Java, PHP or Ruby. Do some more research to find out what kinds of companies or types of products you'd like to work on to help give you an opinion on the factors that differentiate these languges.";
     } else if (smallLarge === "large" && appWeb === "undecided" && netAndroid === "undecided") {
+      imageIcon = "research";
       return "You've narrowed down your options to C#, Java, and PHP because you want to work on the back end for a large company. Now figure out if you want to work on apps or on web products.";
     } else if (appWeb === "app" && netAndroid === "undecided") {
+      imageIcon = "research";
       return "You've now narrowed down your options to C# and Java because you want to have the ability to work on apps for large companies. Spend some time learning whether you would prefer to work with .NET or Android.";
     } else if (smallLarge === "small") {
       imageIcon = "ruby";
@@ -31,6 +37,7 @@ var trackSelect = function (compPeople, design, smallLarge, appWeb, netAndroid) 
       return "Java/Android! This track teaches you all about Android. This operating system can be used to create all the apps you can think of.";
     }
   } else {
+    imageIcon = "error"
     return "Please make sure you answer every question.";
   }
 };
@@ -48,7 +55,6 @@ $(document).ready(function(){
 
     $("#result").text(trackSelect(computersAnswer, cssAnswer, rubyAnswer, phpAnswer, javaCSharpAnswer));
     $("#nameHere").text(nameInput);
-
 
     $("#" + imageIcon).show();
 
